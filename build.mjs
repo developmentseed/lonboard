c1ec09d97eec7962d177a773b5c85a34772c5578
Kyle Barron <kyle@developmentseed.org>
2023-10-02 18:40:46 -0400

    remove other js entry opints

--- build.mjs
+++ build.mjs
@@ -3,13 +3,7 @@ import esbuild from "esbuild";
 // import { wasmLoader } from "esbuild-plugin-wasm";
 
 esbuild.build({
-  entryPoints: [
-    "./src/index.tsx",
-    "./src/point.tsx",
-    "./src/linestring.tsx",
-    "./src/polygon.tsx",
-    "./src/polygon-layer.ts",
-  ],
+  entryPoints: ["./src/index.tsx"],
   bundle: true,
   minify: true,
   target: ["es2020"],
