9490a8164042c84b3d4bafa74d99e9643a432450
Kyle Barron <kyle@developmentseed.org>
2023-10-02 12:42:55 -0400

    Minify JS bundle (#29)

--- build.mjs
+++ build.mjs
@@ -1,11 +1,13 @@
 // build.js
 import esbuild from "esbuild";
-import { wasmLoader } from "esbuild-plugin-wasm";
+// import { wasmLoader } from "esbuild-plugin-wasm";
 
 esbuild.build({
   entryPoints: ["./src/point.tsx", "./src/linestring.tsx", "./src/polygon.tsx"],
   bundle: true,
-  outdir: "deck_widget/static/",
-  plugins: [wasmLoader()],
+  minify: true,
+  target: ["es2020"],
+  outdir: "lonboard/static/",
+  // plugins: [wasmLoader()],
   format: "esm",
 });
