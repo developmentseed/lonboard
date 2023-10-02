9110677dce5af5348ab37a81d8124bd5b4d7b43e
Kyle Barron <kyle@developmentseed.org>
2023-10-02 14:21:44 -0400

    wip pydantic

--- lonboard/widget.py
+++ lonboard/widget.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+import psygnal
 import pathlib
 from io import BytesIO
-
+from anywidget.experimental import widget
 import anywidget
 import geopandas as gpd
 import pyarrow as pa
 import pyarrow.feather as feather
+from pydantic import BaseModel
 import traitlets
 
 from lonboard.geoarrow.geopandas_interop import geopandas_to_geoarrow
@@ -15,6 +17,33 @@ from lonboard.geoarrow.geopandas_interop import geopandas_to_geoarrow
 bundler_output_dir = pathlib.Path(__file__).parent / "static"
 _css = bundler_output_dir / "styles.css"
 
+
+@widget(esm=bundler_output_dir / "polygon.js")
+@psygnal.evented
+class PolygonModel(BaseModel):
+    table_buffer: bytes
+
+    def _get_anywidget_state(self, include):
+        state = self.model_dump(exclude={"table_buffer"}, mode="json")
+        state["table_buffer"] = self.table_buffer
+        return state
+
+
+    @classmethod
+    def from_pyarrow(cls, table: pa.Table, **kwargs) -> PolygonModel:
+        assert (
+            table.schema.field("geometry").metadata.get(b"ARROW:extension:name")
+            == b"geoarrow.polygon"
+        ), "Only Polygon geometries are currently supported by this layer."
+
+        with BytesIO() as bio:
+            feather.write_feather(table, bio, compression="uncompressed")
+            return cls(table_buffer=bio.getvalue(), **kwargs)
+
+    @classmethod
+    def from_geopandas(cls, gdf: gpd.GeoDataFrame, **kwargs) -> PolygonModel:
+        table = geopandas_to_geoarrow(gdf)
+        return cls.from_pyarrow(table, **kwargs)
 
 class PointLayer(anywidget.AnyWidget):
     _esm = bundler_output_dir / "point.js"
