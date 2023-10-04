e395c08bdf92879bac3ab8db0244f0821682433d
Kyle Barron <kyle@developmentseed.org>
2023-10-04 18:04:11 -0400

    Custom traitlet classes for pyarrow table, color/float accessors (#51)
    * wip accessor traits
    
    * flesh out accessor traits
    
    * Lint and working map again
    
    * docstring

--- lonboard/serialization.py
+++ lonboard/serialization.py
@@ -24,18 +24,9 @@ def serialize_table_to_parquet(table: pa.Table) -> bytes:
         return bio.getvalue()
 
 
-def color_ndarray_to_pyarrow(data: NDArray[np.uint8]) -> pa.FixedSizeListArray:
-    assert np.issubdtype(data.dtype, np.uint8), "Color array must be uint8 type."
-    assert data.ndim == 2, "Color array must be 2D."
-
-    list_size = data.shape[1]
-    assert list_size in (3, 4), "Color array must have 3 or 4 as its second dimension."
-
-    return pa.FixedSizeListArray.from_arrays(data.flatten("C"), list_size)
-
-
-def serialize_pyarrow_color(data: pa.FixedSizeListArray) -> bytes:
-    pyarrow_table = pa.table({"color": data})
+def serialize_pyarrow_column(data: pa.Array) -> bytes:
+    """Serialize a pyarrow column to a Parquet file with one column"""
+    pyarrow_table = pa.table({"value": data})
     return serialize_table_to_parquet(pyarrow_table)
 
 
@@ -45,40 +36,29 @@ def serialize_color_accessor(
     if data is None:
         return None
 
-    if isinstance(data, np.ndarray):
-        parr = color_ndarray_to_pyarrow(data)
-        return serialize_pyarrow_color(parr)
+    if isinstance(data, (list, tuple)):
+        return data
 
-    if isinstance(data, (pa.ChunkedArray, pa.Array)):
-        assert pa.types.is_fixed_size_list(data.type)
-        assert data.type.list_size in (3, 4)
-        return serialize_pyarrow_color(data)
-
-    return data
-
-
-def serialize_pyarrow_value_column(data: pa.Array) -> bytes:
-    pyarrow_table = pa.table({"value": data})
-    return serialize_table_to_parquet(pyarrow_table)
+    assert isinstance(data, (pa.ChunkedArray, pa.Array))
+    return serialize_pyarrow_column(data)
 
 
 def serialize_float_accessor(data: Union[int, float, NDArray[np.floating]], obj=None):
     if data is None:
         return None
 
-    if isinstance(data, np.ndarray):
-        assert np.issubdtype(data.dtype, np.number), "Expected numeric dtype"
-        # TODO: should we always be casting to float32? Should it be possible/allowed to
-        # pass in ~int8 or a data type smaller than float32?
-        parr = pa.array(data.astype(np.float32))
-        return serialize_pyarrow_value_column(parr)
+    if isinstance(data, (int, float)):
+        return data
 
-    if isinstance(data, (pa.ChunkedArray, pa.Array)):
-        assert pa.types.is_floating(data.type)
-        return serialize_pyarrow_value_column(data.cast(pa.float32()))
+    assert isinstance(data, (pa.ChunkedArray, pa.Array))
+    return serialize_pyarrow_column(data)
 
-    return data
+
+def serialize_table(data, obj=None):
+    assert isinstance(data, pa.Table), "expected pyarrow table"
+    return serialize_table_to_parquet(data)
 
 
 COLOR_SERIALIZATION = {"to_json": serialize_color_accessor}
 FLOAT_SERIALIZATION = {"to_json": serialize_float_accessor}
+TABLE_SERIALIZATION = {"to_json": serialize_table}
