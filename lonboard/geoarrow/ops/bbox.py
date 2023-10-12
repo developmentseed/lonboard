f880c8ec2fb9a094ff968c1fdd68426976f2c725
Kyle Barron <kyle@developmentseed.org>
2023-10-12 14:14:29 -0400

    geoarrow ops mod (#86)

---
+++ lonboard/geoarrow/ops/bbox.py
@@ -0,0 +1,96 @@
+"""Compute the total bounds of a geoarrow column
+"""
+
+from __future__ import annotations
+
+import math
+from dataclasses import dataclass
+from typing import Tuple
+
+import numpy as np
+import pyarrow as pa
+
+from lonboard.constants import EXTENSION_NAME
+
+
+@dataclass
+class Bbox:
+    minx: float = math.inf
+    miny: float = math.inf
+    maxx: float = -math.inf
+    maxy: float = -math.inf
+
+    def update(self, other: Bbox):
+        if other.minx < self.minx:
+            self.minx = other.minx
+        if other.miny < self.miny:
+            self.miny = other.miny
+        if other.maxx > self.maxx:
+            self.maxx = other.maxx
+        if other.maxy > self.maxy:
+            self.maxy = other.maxy
+
+    def to_tuple(self) -> Tuple[float, float, float, float]:
+        return (self.minx, self.miny, self.maxx, self.maxy)
+
+
+def total_bounds(field: pa.Field, column: pa.ChunkedArray) -> Bbox:
+    """Compute the total bounds of a geometry column"""
+    extension_type_name = field.metadata[b"ARROW:extension:name"]
+
+    if extension_type_name == EXTENSION_NAME.POINT:
+        return _total_bounds_nest_0(column)
+
+    if extension_type_name in [EXTENSION_NAME.LINESTRING, EXTENSION_NAME.MULTIPOINT]:
+        return _total_bounds_nest_1(column)
+
+    if extension_type_name in [EXTENSION_NAME.POLYGON, EXTENSION_NAME.MULTILINESTRING]:
+        return _total_bounds_nest_2(column)
+
+    if extension_type_name == EXTENSION_NAME.MULTIPOLYGON:
+        return _total_bounds_nest_3(column)
+
+    assert False
+
+
+def _coords_bbox(arr: pa.FixedSizeListArray) -> Bbox:
+    np_arr = arr.flatten().to_numpy().reshape(-1, arr.type.list_size)
+    min_vals = np.min(np_arr, axis=0)
+    max_vals = np.max(np_arr, axis=0)
+    return Bbox(minx=min_vals[0], miny=min_vals[1], maxx=max_vals[0], maxy=max_vals[1])
+
+
+def _total_bounds_nest_0(column: pa.ChunkedArray) -> Bbox:
+    bbox = Bbox()
+    for chunk in column.chunks:
+        coords = chunk
+        bbox.update(_coords_bbox(coords))
+
+    return bbox
+
+
+def _total_bounds_nest_1(column: pa.ChunkedArray) -> Bbox:
+    bbox = Bbox()
+    for chunk in column.chunks:
+        coords = chunk.flatten()
+        bbox.update(_coords_bbox(coords))
+
+    return bbox
+
+
+def _total_bounds_nest_2(column: pa.ChunkedArray) -> Bbox:
+    bbox = Bbox()
+    for chunk in column.chunks:
+        coords = chunk.flatten().flatten()
+        bbox.update(_coords_bbox(coords))
+
+    return bbox
+
+
+def _total_bounds_nest_3(column: pa.ChunkedArray) -> Bbox:
+    bbox = Bbox()
+    for chunk in column.chunks:
+        coords = chunk.flatten().flatten().flatten()
+        bbox.update(_coords_bbox(coords))
+
+    return bbox
--- lonboard/viewport.py
+++ lonboard/viewport.py
@@ -8,155 +8,26 @@ under the Apache 2 license.
 from __future__ import annotations
 
 import math
-from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Tuple
 
-import numpy as np
 import pyarrow as pa
 
-from lonboard.constants import EXTENSION_NAME
+from lonboard.geoarrow.ops.bbox import Bbox, total_bounds
+from lonboard.geoarrow.ops.centroid import WeightedCentroid, weighted_centroid
 from lonboard.utils import get_geometry_column_index
-
-
-@dataclass
-class Bbox:
-    minx: float = math.inf
-    miny: float = math.inf
-    maxx: float = -math.inf
-    maxy: float = -math.inf
-
-    def update(self, other: Bbox):
-        if other.minx < self.minx:
-            self.minx = other.minx
-        if other.miny < self.miny:
-            self.miny = other.miny
-        if other.maxx > self.maxx:
-            self.maxx = other.maxx
-        if other.maxy > self.maxy:
-            self.maxy = other.maxy
-
-    def to_tuple(self) -> Tuple[float, float, float, float]:
-        return (self.minx, self.miny, self.maxx, self.maxy)
-
-
-@dataclass
-class WeightedCentroid:
-    # Existing average for x and y
-    x: Optional[float] = None
-    y: Optional[float] = None
-    num_items: int = 0
-
-    def update(self, coords: pa.FixedSizeListArray):
-        """Update the average for x and y based on a new chunk of data
-
-        Note that this does not keep a cumulative sum due to precision concerns. Rather
-        it incrementally updates based on a delta, and never multiplies to large
-        constant values.
-
-        Note: this currently computes the mean weighted _per coordinate_ and not _per
-        geometry_.
-        """
-        np_arr = coords.flatten().to_numpy().reshape(-1, coords.type.list_size)
-        new_chunk_len = np_arr.shape[0]
-
-        if self.x is None or self.y is None:
-            assert self.x is None and self.y is None and self.num_items == 0
-            self.x = np.mean(np_arr[:, 0])
-            self.y = np.mean(np_arr[:, 1])
-            self.num_items = new_chunk_len
-            return
-
-        existing_modifier = self.num_items / (self.num_items + new_chunk_len)
-        new_chunk_modifier = new_chunk_len / (self.num_items + new_chunk_len)
-
-        new_chunk_avg_x = np.mean(np_arr[:, 0])
-        new_chunk_avg_y = np.mean(np_arr[:, 0])
-
-        existing_x_avg = self.x
-        existing_y_avg = self.y
-
-        self.x = (
-            existing_x_avg * existing_modifier + new_chunk_avg_x * new_chunk_modifier
-        )
-        self.y = (
-            existing_y_avg * existing_modifier + new_chunk_avg_y * new_chunk_modifier
-        )
-        self.num_items += new_chunk_len
 
 
 def get_bbox_center(table: pa.Table) -> Tuple[Bbox, WeightedCentroid]:
     """Get the bounding box and geometric (weighted) center of the geometries in the
     table."""
     geom_col_idx = get_geometry_column_index(table.schema)
+    geom_field = table.schema.field(geom_col_idx)
     geom_col = table.column(geom_col_idx)
-    extension_type_name = table.schema.field(geom_col_idx).metadata[
-        b"ARROW:extension:name"
-    ]
 
-    if extension_type_name == EXTENSION_NAME.POINT:
-        return _get_bbox_center_nest_0(geom_col)
+    bbox = total_bounds(geom_field, geom_col)
+    centroid = weighted_centroid(geom_field, geom_col)
 
-    if extension_type_name in [EXTENSION_NAME.LINESTRING, EXTENSION_NAME.MULTIPOINT]:
-        return _get_bbox_center_nest_1(geom_col)
-
-    if extension_type_name in [EXTENSION_NAME.POLYGON, EXTENSION_NAME.MULTILINESTRING]:
-        return _get_bbox_center_nest_2(geom_col)
-
-    if extension_type_name == EXTENSION_NAME.MULTIPOLYGON:
-        return _get_bbox_center_nest_3(geom_col)
-
-    assert False
-
-
-def _coords_bbox(arr: pa.FixedSizeListArray) -> Bbox:
-    np_arr = arr.flatten().to_numpy().reshape(-1, arr.type.list_size)
-    min_vals = np.min(np_arr, axis=0)
-    max_vals = np.max(np_arr, axis=0)
-    return Bbox(minx=min_vals[0], miny=min_vals[1], maxx=max_vals[0], maxy=max_vals[1])
-
-
-def _get_bbox_center_nest_0(column: pa.ChunkedArray) -> Tuple[Bbox, WeightedCentroid]:
-    bbox = Bbox()
-    centroid = WeightedCentroid()
-    for chunk in column.chunks:
-        coords = chunk
-        bbox.update(_coords_bbox(coords))
-        centroid.update(coords)
-
-    return (bbox, centroid)
-
-
-def _get_bbox_center_nest_1(column: pa.ChunkedArray) -> Tuple[Bbox, WeightedCentroid]:
-    bbox = Bbox()
-    centroid = WeightedCentroid()
-    for chunk in column.chunks:
-        coords = chunk.flatten()
-        bbox.update(_coords_bbox(coords))
-        centroid.update(coords)
-
-    return (bbox, centroid)
-
-
-def _get_bbox_center_nest_2(column: pa.ChunkedArray) -> Tuple[Bbox, WeightedCentroid]:
-    bbox = Bbox()
-    centroid = WeightedCentroid()
-    for chunk in column.chunks:
-        coords = chunk.flatten().flatten()
-        bbox.update(_coords_bbox(coords))
-        centroid.update(coords)
-
-    return (bbox, centroid)
-
-
-def _get_bbox_center_nest_3(column: pa.ChunkedArray) -> Tuple[Bbox, WeightedCentroid]:
-    bbox = Bbox()
-    centroid = WeightedCentroid()
-    for chunk in column.chunks:
-        coords = chunk.flatten().flatten().flatten()
-        bbox.update(_coords_bbox(coords))
-        centroid.update(coords)
-
-    return (bbox, centroid)
+    return bbox, centroid
 
 
 def bbox_to_zoom_level(bbox: Bbox) -> int:
