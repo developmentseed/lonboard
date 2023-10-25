c690e1ef104464b6498b587f1cfecd3b2e28dcd7
Vitor George <vitor.george@gmail.com>
2023-10-25 16:47:05 +0100

    Small refactor

--- src/tooltip/index.ts
+++ src/tooltip/index.ts
@@ -3,9 +3,25 @@ import { TooltipContent } from "@deck.gl/core/typed/lib/tooltip";
 
 import "./index.css";
 
+function toHtmlTable(featureProperties: Record<string, any>): string {
+  return `<table>
+      <tbody>
+        ${Object.keys(featureProperties)
+          .map((key) => {
+            const value = featureProperties[key];
+            return `<tr>
+              <td>${key}</td>
+              <td>${value}</td>
+            </tr>`;
+          })
+          .join("")}
+      </tbody>
+    </table>`;
+}
+
 export function getTooltip({ object }: PickingInfo): TooltipContent {
   if (object) {
-    const sampleFeature = {
+    const sampleFeatureProperties = {
       timestamp: new Date().toISOString(),
       url: "https://github.com/developmentseed/lonboard/",
       version: 1.2,
@@ -16,24 +32,12 @@ export function getTooltip({ object }: PickingInfo): TooltipContent {
       integer: 9007199254740991,
     };
 
-    function toHtmlTable(featureProperties) {
-      return `<table>
-          <tbody>
-            ${Object.keys(featureProperties)
-              .map((key) => {
-                const value = featureProperties[key];
-                return `<tr>
-                  <td>${key}</td>
-                  <td>${value}</td>
-                </tr>`;
-              })
-              .join("")}
-          </tbody>
-        </table>`;
-    }
-
     return {
-      html: `<div class="lonboard-tooltip">${toHtmlTable(sampleFeature)}</div>`,
+      html: `<div class="lonboard-tooltip">${toHtmlTable(
+        sampleFeatureProperties
+      )}</div>`,
     };
   }
+
+  return null;
 }
