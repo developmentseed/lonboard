48860539600b6fea701d24c3f3214fcc83be0936
Kyle Barron <kyle@developmentseed.org>
2023-11-03 17:41:58 -0400

    Save widget to standalone HTML file

--- build.mjs
+++ build.mjs
@@ -1,15 +1,15 @@
 // build.js
 import esbuild from "esbuild";
-// import { wasmLoader } from "esbuild-plugin-wasm";
 
 esbuild.build({
-  entryPoints: [
-    "./src/index.tsx",
-  ],
+  entryPoints: ["./src/index.tsx"],
   bundle: true,
   minify: true,
   target: ["es2020"],
   outdir: "lonboard/static/",
-  // plugins: [wasmLoader()],
   format: "esm",
+  // Ref https://github.com/manzt/anywidget/issues/369#issuecomment-1792376003
+  define: {
+    "define.amd": "false",
+  },
 });
