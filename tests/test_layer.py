870192655e0ee59bd9ee647611deb3824be68517
Kyle Barron <kyle@developmentseed.org>
2023-11-06 15:42:41 -0500

    Error when creating class with unknown keyword argument (#209)

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -19,3 +19,11 @@ def test_accessor_length_validation():
         _layer = ScatterplotLayer.from_geopandas(gdf, get_radius=np.array([1, 2, 3]))
 
     _layer = ScatterplotLayer.from_geopandas(gdf, get_radius=np.array([1, 2]))
+
+
+def test_layer_fails_with_unexpected_argument():
+    points = shapely.points([1, 2], [3, 4])
+    gdf = gpd.GeoDataFrame(geometry=points)
+
+    with pytest.raises(TypeError, match="unexpected keyword argument"):
+        ScatterplotLayer.from_geopandas(gdf, unknown_keyword="foo")
