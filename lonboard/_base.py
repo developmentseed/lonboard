e0f4cc36a1e18989e22599a1930e25916c379e25
Kyle Barron <kyle@developmentseed.org>
2023-11-06 20:07:17 -0500

    Fix unknown argument for inherited props (#213)

--- lonboard/_base.py
+++ lonboard/_base.py
@@ -5,7 +5,9 @@ from ipywidgets import Widget
 class BaseWidget(Widget):
     def __init__(self, **kwargs):
         # Raise error on unknown keyword name
-        layer_trait_names = self.class_own_traits().keys()
+        # Note: we don't use `class_own_traits()` because some layer props are set on
+        # BaseLayer
+        layer_trait_names = self.trait_names()
         for provided_trait_name in kwargs.keys():
             if provided_trait_name not in layer_trait_names:
                 raise TypeError(f"unexpected keyword argument '{provided_trait_name}'")
@@ -16,7 +18,7 @@ class BaseWidget(Widget):
 class BaseAnyWidget(AnyWidget):
     def __init__(self, **kwargs):
         # Raise error on unknown keyword name
-        layer_trait_names = self.class_own_traits().keys()
+        layer_trait_names = self.trait_names()
         for provided_trait_name in kwargs.keys():
             if provided_trait_name not in layer_trait_names:
                 raise TypeError(f"unexpected keyword argument '{provided_trait_name}'")
