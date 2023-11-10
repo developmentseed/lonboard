863cd1a4b72e376cabc2afa10b0b75850d14cd02
Kyle Barron <kyle@developmentseed.org>
2023-11-10 14:05:33 -0500

    Update migration notebook

--- lonboard/experimental/__init__.py
+++ lonboard/experimental/__init__.py
@@ -1,1 +1,8 @@
-from ._layer import ArcLayer
+"""Experimental layers for lonboard
+
+These layers have not been as well tested as other layers. You may encounter crashes or
+unexpected behavior when using them.
+"""
+
+from ._layer import ArcLayer, TextLayer
+from .layer_extension import BrushingExtension, CollisionFilterExtension
