72b66848dca62cfbb390fdb48085adf97c1d8723
Kyle Barron <kyle@developmentseed.org>
2023-12-04 12:13:55 -0500

    fix picking first row

--- src/tooltip/index.ts
+++ src/tooltip/index.ts
@@ -31,7 +31,8 @@ export function getTooltip({ object }: GeoArrowPickingInfo): TooltipContent {
     // Without this block, we end up showing a tooltip even when not hovering
     // over a point
     if (
-      !object[rowIndexSymbol] ||
+      object[rowIndexSymbol] === null ||
+      object[rowIndexSymbol] === undefined ||
       (object[rowIndexSymbol] && object[rowIndexSymbol] < 0)
     ) {
       return null;
