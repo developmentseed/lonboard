7170195b7619392fb2279ee4c223790f2ec2040a
Kyle Barron <kyle@developmentseed.org>
2024-01-29 18:04:39 -0500

    geoarrow-based multithreaded coordinate reprojection (#337)
    ### Change list
    
    - Include PROJJSON CRS on geometry array metadata when converting from
    GeoPandas
    - Move CRS checking and reprojection into GeoArrow level instead of
    GeoPandas level
    
    ### Example
    
    Low level example and benchmark. 4.6x reprojection speedup across 8
    threads!
    
    ```py
    import geodatasets
    import pyarrow as pa
    import geopandas as gpd
    import numpy as np
    
    from lonboard._geoarrow.geopandas_interop import geopandas_to_geoarrow
    from lonboard._geoarrow.ops import reproject
    
    
    path = geodatasets.get_path('nybb')
    gdf = gpd.read_file(path)
    table = geopandas_to_geoarrow(gdf)
    
    field = table.schema.field(4)
    column = table['geometry']
    chunk = column.chunk(0)
    
    new_chunked_array = pa.chunked_array([chunk] * 100)
    %timeit out = reproject(field, new_chunked_array, max_workers=8)
    # 693 ms ± 85.6 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
    %timeit out = reproject(field, new_chunked_array, max_workers=1)
    # 3.22 s ± 22 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
    ```

--- lonboard/_geoarrow/geopandas_interop.py
+++ lonboard/_geoarrow/geopandas_interop.py
@@ -18,5 +18,9 @@ def geopandas_to_geoarrow(
         df_attr = df_attr[columns]
 
     table = pa.Table.from_pandas(df_attr, preserve_index=preserve_index)
-    field, geom_arr = construct_geometry_array(np.array(gdf.geometry))
+    field, geom_arr = construct_geometry_array(
+        np.array(gdf.geometry),
+        crs_str=gdf.crs.to_json() if gdf.crs is not None else None,
+    )
+
     return table.append_column(field, geom_arr)
