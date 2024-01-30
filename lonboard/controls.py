54a2d649c235c6831351ba3bb32ce580dd7ca3b7
Kyle Barron <kyle@developmentseed.org>
2024-01-30 13:08:42 -0500

    Update docs

--- lonboard/controls.py
+++ lonboard/controls.py
@@ -2,16 +2,60 @@ from functools import partial
 from typing import Sequence
 
 import traitlets
-from ipywidgets import FloatRangeSlider, VBox
+from ipywidgets import FloatRangeSlider
 from ipywidgets.widgets.trait_types import TypedTuple
+
+# Import from source to allow mkdocstrings to link to base class
+from ipywidgets.widgets.widget_box import VBox
 
 
 class MultiRangeSlider(VBox):
     """A widget for multiple ranged sliders.
 
-    This is designed to be used with the DataFilterExtension
+    This is designed to be used with the
+    [DataFilterExtension][lonboard.experimental.DataFilterExtension] when you want to
+    filter on 2 to 4 columns on the same time.
 
-    If you have only a single filter, use an upstream FloatRangeSlider directly.
+    If you have only a single filter, use an ipywidgets
+    [FloatRangeSlider][ipywidgets.widgets.widget_float.FloatRangeSlider] directly.
+
+    # Example
+
+    ```py
+    from ipywidgets import FloatRangeSlider
+
+    slider1 = FloatRangeSlider(
+        value=(2, 5),
+        min=0,
+        max=10,
+        step=0.1,
+        description="First slider: "
+    )
+    slider2 = FloatRangeSlider(
+        value=(30, 40),
+        min=0,
+        max=50,
+        step=1,
+        description="Second slider: "
+    )
+    multi_slider = MultiRangeSlider([slider1, slider2])
+    multi_slider
+    ```
+
+    Then to propagate updates to a rendered layer, call
+    [jsdlink][ipywidgets.widgets.widget_link.jsdlink] to connect the two widgets.
+
+    ```py
+    from ipywidgets import jsdlink
+
+    jsdlink(
+        (multi_slider, "value"),
+        (layer, "filter_range")
+    )
+    ```
+
+    As you change the slider, the `filter_range` value on the layer class should be
+    updated.
     """
 
     # We use a tuple to force reassignment to update the list
