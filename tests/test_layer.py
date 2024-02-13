05f645e8bde9569686d7264a276d2db5e1e13b19
Kyle Barron <kyle@developmentseed.org>
2024-02-13 12:33:34 -0500

    fix error case

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -48,7 +48,7 @@ def test_layer_fails_with_unexpected_argument():
     points = shapely.points([1, 2], [3, 4])
     gdf = gpd.GeoDataFrame(geometry=points)
 
-    with pytest.raises(TypeError, match="unexpected keyword argument"):
+    with pytest.raises(TypeError, match="Unexpected keyword argument"):
         _layer = ScatterplotLayer.from_geopandas(gdf, unknown_keyword="foo")
 
 
