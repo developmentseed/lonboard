ddea3e276e5d264660ee5d93f2f5dfb4acce84d6
Kyle Barron <kyle@developmentseed.org>
2024-03-20 16:20:50 -0400

    Fix viz() with geo interface input

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -13,7 +13,7 @@ def test_viz_wkb_geoarrow():
 
 
 def test_viz_reproject():
-    gdf = gpd.read_file(gpd.datasets.get_path("nybb"))
+    gdf = gpd.read_file(geodatasets.get_path("nybb"))
     map_ = viz(gdf)
 
     # Assert table was reprojected
@@ -21,3 +21,19 @@ def test_viz_reproject():
     expected_coord = -74.05050951794041, 40.56643026026788
     assert (first_coord[0].as_py() - expected_coord[0]) < 0.0001
     assert (first_coord[1].as_py() - expected_coord[1]) < 0.0001
+
+
+def test_viz_geo_interface():
+    class GeoInterfaceHolder:
+        def __init__(self, geom) -> None:
+            self.geom = geom
+
+        @property
+        def __geo_interface__(self):
+            return self.geom.__geo_interface__
+
+    gdf = gpd.read_file(geodatasets.get_path("nybb")).to_crs("EPSG:4326")
+    geo_interface_obj = GeoInterfaceHolder(gdf.geometry[0])
+    map_ = viz(geo_interface_obj)
+
+    assert isinstance(map_.layers[0], SolidPolygonLayer)
