fd0a81bb9e1b3d6d92d76c8f244fa2bca678565e
Kyle Barron <kyle@developmentseed.org>
2024-08-19 13:51:30 -0400

    Add docs link to matplotlib's list of named colors

--- lonboard/traits.py
+++ lonboard/traits.py
@@ -227,7 +227,9 @@ class ColorAccessor(FixedErrorTraitType):
     - A `list` or `tuple` with three or four integers, ranging between 0 and 255
       (inclusive). This will be used as the color for all objects.
     - A `str` representing a hex color or "well known" color interpretable by
-      [matplotlib.colors.to_rgba][matplotlib.colors.to_rgba].
+      [matplotlib.colors.to_rgba][matplotlib.colors.to_rgba]. See also matplotlib's
+      [list of named
+      colors](https://matplotlib.org/stable/gallery/color/named_colors.html#base-colors).
     - A numpy `ndarray` with two dimensions and data type [`np.uint8`][numpy.uint8]. The
       size of the second dimension must be `3` or `4`, and will correspond to either RGB
       or RGBA colors.
