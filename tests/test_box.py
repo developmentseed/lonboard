311dd917f779d9b23b1a9bdc6706212f5d16cf22
Kyle Barron <kyle@developmentseed.org>
2025-06-16 18:30:45 -0400

    feat: Support `geoarrow.box` arrays from GeoArrow 0.2 spec (#793)
    ```py
    import geoarrow.pyarrow as ga
    
    from lonboard import viz
    
    arr = ga.box(
        [
            "LINESTRING (0 10, 34 -1)",
            "LINESTRING (10 20, 44 -10)",
            "LINESTRING (20 40, 54 5)",
        ],
    )
    
    viz(arr)
    ```
    
    <img width="513" alt="image"
    src="https://github.com/user-attachments/assets/7ff999d7-97c4-4845-b0af-3e0209e39f76"
    />
    
    Closes #724

---
+++ tests/test_box.py
@@ -0,0 +1,43 @@
+import geoarrow.pyarrow as ga
+from arro3.core import Table
+
+from lonboard import PolygonLayer, SolidPolygonLayer, viz
+
+
+def test_viz_box():
+    arr = ga.box(
+        [
+            "LINESTRING (0 10, 34 -1)",
+            "LINESTRING (10 20, 44 -10)",
+            "LINESTRING (20 40, 54 5)",
+        ],
+    )
+
+    m = viz(arr)
+    assert isinstance(m.layers[0], PolygonLayer)
+
+
+def test_viz_box_polygon_layer():
+    arr = ga.box(
+        [
+            "LINESTRING (0 10, 34 -1)",
+            "LINESTRING (10 20, 44 -10)",
+            "LINESTRING (20 40, 54 5)",
+        ],
+    )
+    table = Table.from_arrays([arr], names=["geometry"])  # type: ignore
+    assert (
+        table.field("geometry").metadata_str["ARROW:extension:name"] == "geoarrow.box"
+    )
+
+    layer = PolygonLayer(table=table)
+    assert (
+        layer.table.field("geometry").metadata_str["ARROW:extension:name"]
+        == "geoarrow.polygon"
+    )
+
+    layer2 = SolidPolygonLayer(table=table)
+    assert (
+        layer2.table.field("geometry").metadata_str["ARROW:extension:name"]
+        == "geoarrow.polygon"
+    )
