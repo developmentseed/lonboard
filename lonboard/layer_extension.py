e4625c20d61c0b9cae0e4d0ee365fd5aeed9e765
ATL2001 <35881864+ATL2001@users.noreply.github.com>
2025-10-15 19:08:39 -0500

    change min values to 1 and set default to 1 for filter_size

--- lonboard/layer_extension.py
+++ lonboard/layer_extension.py
@@ -360,7 +360,7 @@ class DataFilterExtension(BaseExtension):
         ),
     }
 
-    filter_size = t.Int(None, min=0, max=4, allow_none=True).tag(sync=True)
+    filter_size = t.Int(None, min=1, max=4, allow_none=True).tag(sync=True)
     """The size of the filter (number of columns to filter by).
 
     The data filter can show/hide data based on 1-4 numeric properties of each object.
@@ -369,7 +369,7 @@ class DataFilterExtension(BaseExtension):
     - Default 1.
     """
 
-    category_size = t.Int(None, min=0, max=4, allow_none=True).tag(sync=True)
+    category_size = t.Int(None, min=1, max=4, allow_none=True).tag(sync=True)
     """The size of the category filter (number of columns to filter by).
 
     The category filter can show/hide data based on 1-4 properties of each object.
