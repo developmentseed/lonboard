8a7665aae685d935375d1c3e0b8d7c4b4523cd9e
Kyle Barron <kyle@developmentseed.org>
2025-10-27 19:47:26 -0400

    Move timestamp accessor

--- lonboard/traits/__init__.py
+++ lonboard/traits/__init__.py
@@ -5,4 +5,12 @@ documentation on how to define new traitlet types.
 """
 
 from ._base import FixedErrorTraitType
+from ._color import ColorAccessor
+from ._extensions import DashArrayAccessor, FilterValueAccessor
+from ._float import FloatAccessor
+from ._map import BasemapUrl, HeightTrait, ViewStateTrait
+from ._normal import NormalAccessor
+from ._point import PointAccessor
 from ._table import ArrowTableTrait
+from ._text import TextAccessor
+from ._timestamp import TimestampAccessor
