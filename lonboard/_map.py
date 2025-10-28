1ec97a807481999fe7a63337197f57e9ffe543fe
Kyle Barron <kyle@developmentseed.org>
2025-10-27 23:25:32 -0400

    feat: Support for map controls (#924)
    ### Change list
    
    - Adds definitions for map "controls" (which deck.gl calls "widgets",
    but the name "widget" is too overloaded in the context of Jupyter, so
    I'd like to call them "controls")
    - Support for `ScaleControl`, `NavigationControl`, `FullscreenControl`
    - These controls are supported by both deck.gl and maplibre, and so
    implementations are defined for creating both the deck.gl and maplibre
    counterparts
    
    ### Example
    
    ```py
    from lonboard import Map
    from lonboard.controls import ScaleControl, FullscreenControl, NavigationControl
    from lonboard.basemap import MaplibreBasemap
    
    m = Map([], 
        basemap=MaplibreBasemap(mode="overlaid"),
        controls=[ScaleControl(), FullscreenControl(), NavigationControl()]
    )
    m
    ```
    
    <img width="734" height="506" alt="image"
    src="https://github.com/user-attachments/assets/42fb5e55-57d8-4b2a-8fe2-f70b804dabce"
    />
    
    We should probably change the default positioning of controls, move the
    bbox selector, or make the bbox selector a control itself.
    
    ----
    
    However, for deck.gl-driven rendering, I can only see the scale control:
    <img width="756" height="611" alt="image"
    src="https://github.com/user-attachments/assets/f8e6be57-55ec-45be-9c14-cf76b4ea64d0"
    />
    
    Logged in https://github.com/developmentseed/lonboard/issues/990
    
    ### Todo list
    
    - Figure out how to pass these react components as children of the
    `OverlayRenderer` or `DeckFirstRenderer` (depends on
    https://github.com/developmentseed/lonboard/pull/921)
    - Use the refactored model initialization from
    https://github.com/developmentseed/lonboard/pull/923 to create the list
    of control instances on the deck side
    - Integrate more cleanly into the Python API (an extension of
    https://github.com/developmentseed/lonboard/pull/908)
    
    Closes https://github.com/developmentseed/lonboard/issues/842, relevant
    to https://github.com/developmentseed/lonboard/issues/681

--- lonboard/_map.py
+++ lonboard/_map.py
@@ -13,6 +13,7 @@ from lonboard._base import BaseAnyWidget
 from lonboard._html_export import map_to_html
 from lonboard._viewport import compute_view
 from lonboard.basemap import CartoStyle, MaplibreBasemap
+from lonboard.controls import BaseControl
 from lonboard.layer import BaseLayer
 from lonboard.traits import HeightTrait, VariableLengthTuple, ViewStateTrait
 from lonboard.traits._map import DEFAULT_INITIAL_VIEW_STATE
@@ -191,6 +192,12 @@ class Map(BaseAnyWidget):
     )
     """One or more `Layer` objects to display on this map.
     """
+
+    controls = VariableLengthTuple(t.Instance(BaseControl)).tag(
+        sync=True,
+        **ipywidgets.widget_serialization,
+    )
+    """One or more map controls to display on this map."""
 
     views: t.Instance[BaseView | None] = t.Instance(BaseView, allow_none=True).tag(
         sync=True,
