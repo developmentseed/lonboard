9c200de9d2e1c353f51773d1bdcd9d85330ba9fb
Kyle Barron <kyle@developmentseed.org>
2025-10-29 16:19:12 -0400

    add polygon constructor for clarity

--- src/model/layer/polygon.ts
+++ src/model/layer/polygon.ts
@@ -183,6 +183,10 @@ export abstract class BasePolygonModel extends BaseArrowLayerModel {
 export class PolygonModel extends BasePolygonModel {
   static layerType = "polygon";
 
+  constructor(model: WidgetModel, updateStateCallback: () => void) {
+    super(model, updateStateCallback);
+  }
+
   layerProps(batchIndex: number): GeoArrowPolygonLayerProps {
     return this.basePolygonLayerProps(batchIndex);
   }
