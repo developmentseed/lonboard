d733fce5c13ebc9d22f8620031b72872c1189686
ATL2001 <35881864+ATL2001@users.noreply.github.com>
2025-11-01 18:45:13 -0500

    move FilterCategoryAccessor to _extensions.py

--- lonboard/traits/__init__.py
+++ lonboard/traits/__init__.py
@@ -7,7 +7,7 @@ documentation on how to define new traitlet types.
 from ._a5 import A5Accessor
 from ._base import FixedErrorTraitType, VariableLengthTuple
 from ._color import ColorAccessor
-from ._extensions import DashArrayAccessor, FilterValueAccessor
+from ._extensions import DashArrayAccessor, FilterCategoryAccessor, FilterValueAccessor
 from ._float import FloatAccessor
 from ._h3 import H3Accessor
 from ._map import BasemapUrl, HeightTrait, ViewStateTrait
@@ -23,6 +23,7 @@ __all__ = [
     "BasemapUrl",
     "ColorAccessor",
     "DashArrayAccessor",
+    "FilterCategoryAccessor",
     "FilterValueAccessor",
     "FixedErrorTraitType",
     "FloatAccessor",
