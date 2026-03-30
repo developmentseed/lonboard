2a47107a70639745c779a51f62b9e4bf3f36485a
Kyle Barron <kyle@developmentseed.org>
2026-03-30 15:54:53 -0400

    remove reshape_as_image

--- lonboard/raster/__init__.py
+++ lonboard/raster/__init__.py
@@ -1,6 +1,5 @@
 """Raster support for Lonboard."""
 
 from ._models import EncodedImage
-from ._utils import reshape_as_image
 
-__all__ = ["EncodedImage", "reshape_as_image"]
+__all__ = ["EncodedImage"]
